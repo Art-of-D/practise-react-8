70a454d7ad34865ae0473c3de07266abf2baa9d0
Artur Didur <didur.art@gmail.com>
2025-06-05 23:42:44 +0200

    middle res

--- cart/src/Context.jsx
+++ cart/src/Context.jsx
@@ -1,18 +1,33 @@
 import { useContext, useReducer, useEffect, createContext } from "react";
+import cartItems from "./data";
 import reducer from "./reducer/reducer";
+import {
+  CLEAR_CART,
+  REMOVE,
+  INCREASE,
+  DECREASE,
+  DISPLAY_ITEMS,
+} from "./reducer/actions";
 
 const AppContext = createContext();
 
 const initialState = {
   loading: false,
-  cart: [],
+  cart: new Map(cartItems.map((item) => [item.id, item])),
   total: 0,
   amount: 0,
 };
 
 export const AppProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initialState);
-  return <AppContext.Provider value="hello">{children}</AppContext.Provider>;
+  const getCart = () => {
+    dispatch({ type: DISPLAY_ITEMS });
+  };
+  return (
+    <AppContext.Provider value={{ cart: state.cart, getCart }}>
+      {children}
+    </AppContext.Provider>
+  );
 };
 
 export const useGlobalContext = () => {
--- cart/src/reducer/reducer.js
+++ cart/src/reducer/reducer.js
@@ -1,4 +1,37 @@
 const reducer = (state, action) => {
+  if (action.type === "CLEAR_CART") {
+    return { ...state, cart: [] };
+  }
+  if (action.type === "REMOVE") {
+    return {
+      ...state,
+      cart: state.cart.filter((item) => item.id !== action.payload.id),
+    };
+  }
+  if (action.type === "INCREASE") {
+    let tempCart = state.cart.map((item) => {
+      if (item.id === action.payload.id) {
+        return { ...item, amount: item.amount + 1 };
+      }
+      return item;
+    });
+    return { ...state, cart: tempCart };
+  }
+  if (action.type === "DECREASE") {
+    let tempCart = state.cart.map((item) => {
+      if (item.id === action.payload.id) {
+        return { ...item, amount: item.amount - 1 };
+      }
+      return item;
+    });
+    return { ...state, cart: tempCart };
+  }
+  if (action.type === "LOADING") {
+    return { ...state, loading: true };
+  }
+  if (action.type === "DISPLAY_ITEMS") {
+    return { ...state, cart: action.payload.cart, loading: false };
+  }
   return state;
 };
 
